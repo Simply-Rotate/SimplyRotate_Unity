501d913a2a1bab7498178b01c4f484cdbee5ea07
lolxu <davidx@usc.edu>
2023-04-09 13:53:23 -0700

    added speed up, added smoothing to rotate bar

--- Assets/Scripts/ControlScript.cs
+++ Assets/Scripts/ControlScript.cs
@@ -10,11 +10,14 @@ public class ControlScript : MonoBehaviour
     public bool isMenu = false;
     private float totalDegrees = 0.0f;
     public Slider rotateBar;
+    private float speedUpFactor = 3.0f;
 
     private Rigidbody2D rb;
     private Vector3 myDir;
     private BoxCollider2D myCol;
     private bool isInside = false;
+    private float rotateFactor = 0.0f;
+    private bool canSpeedUp = false;
 
     private void Start()
     {
@@ -56,29 +59,64 @@ public class ControlScript : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            canSpeedUp = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            canSpeedUp = false;
+        }
+    }
+
     void FixedUpdate()
     {
         /*float funny = Input.GetAxis("Vertical");*/
         float turn = Input.GetAxis("Horizontal");
         /*transform.Rotate(-funny * rotateSpeed, 0f, -turn * rotateSpeed);*/
         
+        if (turn == 0f)
+        {
+            rotateFactor = 0.0f;
+        }
+
         if (!isMenu)
         {
             rotateBar.value = totalDegrees;
             if (totalDegrees >= 0.0f)
             {
                 transform.Rotate(0f, 0f, -turn * rotateSpeed);
-                totalDegrees -= Mathf.Abs(turn);
+                rotateFactor += Mathf.Abs(turn) * 0.5f;
+                if (rotateFactor >= 0.95f)
+                {
+                    totalDegrees -= Mathf.Abs(turn);
+                }
+                else if (rotateFactor < 0.95f)
+                {
+                    totalDegrees -= Mathf.Abs(turn * 0.2f);
+                }
             }
         }
         else
         {
             transform.Rotate(0f, 0f, -turn * rotateSpeed);
         }
+
+        if (canSpeedUp)
+        {
+            DoSpeedUp();
+        }
+        else if (!canSpeedUp)
+        {
+            Time.timeScale = 1.0f;
+            Time.fixedDeltaTime = 0.02f;
+        }
         
 
         // debugging stuff
-        if (Input.GetKey(KeyCode.Alpha1))
+        /*if (Input.GetKey(KeyCode.Alpha1))
         {
             FindObjectOfType<RotationManager>().tag = "OldRotManager";
             SceneManager.LoadScene(0);
@@ -102,11 +140,17 @@ public class ControlScript : MonoBehaviour
         {
             FindObjectOfType<RotationManager>().tag = "OldRotManager";
             SceneManager.LoadScene(10);
-        }
+        }*/
     }
 
     public float GetRotationLeft()
     {
         return totalDegrees;
+    }
+
+    private void DoSpeedUp()
+    {
+        Time.timeScale = speedUpFactor;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
     }
 }
