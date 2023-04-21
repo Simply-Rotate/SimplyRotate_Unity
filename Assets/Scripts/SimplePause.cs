1ef3e65c9b62d978ff2cecfaa83c7c97d0bd8ed3
lolxu <davidx@usc.edu>
2023-04-21 14:05:24 -0700

    added a pause screen

---
+++ Assets/Scripts/SimplePause.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimplePause : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pausePanel;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void QuitToMenu()
+    {
+        FindObjectOfType<LevelLoader>().LoadThisLevel(0);
+    }
+}
